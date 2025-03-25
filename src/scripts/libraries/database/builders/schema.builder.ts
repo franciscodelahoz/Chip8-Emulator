908983e046468561b34404a7a3daf90e45ee1bf5
Francisco De La Hoz <39561227+franciscodelahoz@users.noreply.github.com>
2025-03-24 19:59:14 -0500

    refactor: enhance createStore method to use StoreKey type for keyPath

--- src/scripts/libraries/database/builders/schema.builder.ts
+++ src/scripts/libraries/database/builders/schema.builder.ts
@@ -1,10 +1,12 @@
 import { IndexBuilder } from './index.builder';
-import type { DBSchema, StoreNames } from '../../../types/database';
+import type { DBSchema, StoreKey, StoreNames } from '../../../types/database';
 
 export class SchemaBuilder<Schema extends DBSchema> {
   constructor(private readonly db: IDBDatabase) {}
 
-  createStore<StoreName extends StoreNames<Schema>>(name: StoreName & string, keyPath: string): IndexBuilder<Schema, StoreName> {
+  createStore<
+    StoreName extends StoreNames<Schema>,
+  >(name: StoreName & string, keyPath: StoreKey<Schema, StoreName>): IndexBuilder<Schema, StoreName> {
     const store = this.db.createObjectStore(name, { keyPath });
 
     return new IndexBuilder(store);
